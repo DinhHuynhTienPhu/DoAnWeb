4592e56016e816a46fddeb5f11b35da95eaa59f2
DinhHuynhTienPhu <ff101111011@gmail.com>
2021-11-08 21:12:33 +0700

    template added
    - added template raw to folder "template", DO NOT CHANGE ANYTHING IN THIS FOLDER
    -i used templates to build hbs in views folder, still have some error.
    *note: change all href="assets/abcxyz" to href="/abcxyz" (must remove assets)

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan =require('morgan');
+const bodyparser = require('body-parser')
+const exhbs = require('express-handlebars');
+const path = require('path'); 
+
+const app = express();
+app.use(morgan('tiny'));
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.use(express.static(path.join(__dirname, 'assets')));
+//app.use ('/',express.static(path.resolve("__dirname","assets")));
+//app.use ('assets',express.static(path.resolve("__dirname","assets")));
+
+app.engine('handlebars', exhbs());
+app.set('view engine', 'hbs');
+
+
+app.get('/', (rq,res)=>{
+    res.render('index');
+});
+
+app.listen(3000, ()=>console.log('server is running on local host '+ 3000+' ^_^ '));
