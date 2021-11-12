4d84255c0f8384fc4bb3998809c532b3e87af4e8
trandactoan <73386087+trandactoan@users.noreply.github.com>
2021-11-12 21:59:19 +0700

    Add Product's Comment, route file, delete 4 views.
    -Add Product's comment in product_details.hbs, product_details_Seller.hbs
    -Rename file 1234.hbs as product_details_Seller.hbs
    -Move file product_details_Seller.hbs, product_edit.hbs, product_add.hbs from "views" folder to "Seller" folder, which is child of "views" folder
    -Delete list-views, grid-views, four-col, general views

--- server/routes/router.js
+++ server/routes/router.js
@@ -5,17 +5,12 @@ const route = express.Router();
 //home route
 route.get("/", (req, res) => res.render("index"));
 route.all("/about_us", (req, res) => res.render("about_us"));
-route.get("/cart", (req, res) => res.render("./Buyer/cart"));
 route.get("/compair", (req, res) => res.render("compair"));
 route.get("/footer", (req, res) => res.render("footer")); //just on debug to get footer
 route.get("/forget_password", (req, res) => res.render("forget_password"));
-route.get("/four-col", (req, res) => res.render("four-col"));
 route.get("/three-col", (req, res) => res.render("three-col"));
-route.get("/grid-view", (req, res) => res.render("grid-view"));
-route.get("/general", (req, res) => res.render("general"));
 route.get("/header", (req, res) => res.render("header")); //just 4 debug
 route.get("/index", (req, res) => res.render("index"));
-route.get("/list-view", (req, res) => res.render("list-view"));
 route.get("/login", (req, res) => res.render("login"));
 route.get("/product_details", (req, res) => res.render("product_details"));
 route.get("/products", (req, res) => res.render("products"));
@@ -25,9 +20,12 @@ route.get("/contact", (req, res) => res.render("contact"));
 route.get("/my_account", (req, res) => res.render("my_account"));
 route.get("/searching", (req, res) => res.render("searching"));
 
-//seller
+//Buyer route
+route.get("/cart", (req, res) => res.render("./Buyer/cart"));
+
+//Seller route
 route.get("/product_list", (req, res) => res.render("./Seller/product_list"));
-route.get("/product_edit", (req, res) => res.render("product_edit"));
-route.get("/product_add", (req, res) => res.render("product_add"));
-route.get("/1234", (req, res) => res.render("1234"));
+route.get("/product_edit", (req, res) => res.render("./Seller/product_edit"));
+route.get("/product_add", (req, res) => res.render("./Seller/product_add"));
+route.get("/product_details_Seller", (req, res) => res.render("./Seller/product_details_Seller"));
 module.exports = route;
